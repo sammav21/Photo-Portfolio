d0824f9097d3266621277ab0e759191b005e1a0d
sammav21 <sammav96@gmail.com>
2023-10-16 17:18:16 -0400

    testing new home page for iphone viewport issue

--- photo-portfolio/app/page.jsx
+++ photo-portfolio/app/page.jsx
@@ -16,8 +16,9 @@ const roboto_condensed = Roboto_Condensed({
 export default function Home() {
     const [menuVisible, setMenuVisible] = useState(false);
   return (
-    <div className="home-Wrapper flex-col-center" >
-        <Image src={HOMEPIC} alt="pink sky with clouds" priority={true} style={{width:'100%', height: '100%', objectFit: 'cover', objectPosition: '40% 100%',zIndex: '-1', position: 'absolute'}}/>
+    <>
+    <Image src={HOMEPIC} alt="pink sky with clouds" priority={true} style={{width:'100%', height: '100%', objectFit: 'cover', objectPosition: '40% 100%',zIndex: '-1', position: 'absolute'}}/>
+    <div className="home-Text-Wrapper flex-col w100" >
         <h1 className='home-display-text' style={bebas_neue.style}>Samuel Avilés</h1>
         <div className="homeButtons-Wrapper flex-center">
             <button className="link-Home btn-Margin-Horizontal" onClick={() => setMenuVisible(!menuVisible)}>
@@ -27,12 +28,13 @@ export default function Home() {
               <p className='primary-Heading' style={bebas_neue.style}>About</p>
             </Link>
         </div>
-        <div className={menuVisible ? 'menuVisible': 'menuHidden'} style={roboto_condensed.style}>
+        <div className='homeMenu flex' id={menuVisible ? 'menuVisible': undefined} style={roboto_condensed.style}>
             <Link className='link btn-Padding-Rectangle btn-Home'  href='/main/street'><p className='secondary-Heading'>Street</p></Link>
             <Link className='link btn-Padding-Rectangle btn-Home'  href="/main/people"><p className='secondary-Heading'>People</p></Link>
             <Link className='link btn-Padding-Rectangle btn-Home' href="/main/places"><p className='secondary-Heading'>Places</p></Link>
             <Link className='link btn-Padding-Rectangle btn-Home' href="/main/series"><p className='secondary-Heading'>Series</p></Link>
         </div>
     </div>
+    </>
   )
 }
